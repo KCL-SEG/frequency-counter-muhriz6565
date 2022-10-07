30a7351f79a69ae8c561d09afe8ae0dfad275a07
Muhriz Tauseef <muhriztauseef@gmail.com>
2022-10-07 15:34:25 +0100

    exercise 2, ran python3 -m pytest

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
+
     # Your code goes here
+    for i in items:
+        if str(i) in frequencies:
+            frequencies[str(i)]+=1
+        else:
+            frequencies.update({str(i):1})
+    
     return frequencies
